e13bc63c00373e74ceb24a7168254e28d929e352
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-04-16 21:56:03 +0700

    Start project

---
+++ AboutSpaceX/AboutSpaceX/AboutSpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutSpaceXApp.swift
+//  AboutSpaceX
+//
+//  Created by Chinh on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutSpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
